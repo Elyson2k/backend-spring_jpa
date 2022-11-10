4110724e3c2dbd8ab5b77bafc011f2fbf40f040d
Elyson Vinicius <elysonvinicius@outlook.com>
2022-11-10 10:34:05 -0300

    first commit

---
+++ src/main/java/com/springjpa/project/ProjetonovoApplication.java
@@ -0,0 +1,12 @@
+package com.springjpa.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetonovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetonovoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springjpa/project/ProjetonovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjpa.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetonovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
