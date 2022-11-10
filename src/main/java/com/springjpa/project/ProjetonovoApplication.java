bc2fa309a7dc5b8b546785c6ffc619e301abdb64
Elyson Vinicius <elysonvinicius@outlook.com>
2022-11-10 11:45:06 -0300

    Criando operacao de instanciar

--- src/main/java/com/springjpa/project/ProjetonovoApplication.java
+++ src/main/java/com/springjpa/project/ProjetonovoApplication.java
@@ -1,12 +1,31 @@
 package com.springjpa.project;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.springjpa.project.entities.Categoria;
+import com.springjpa.project.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetonovoApplication {
+public class ProjetonovoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetonovoApplication.class, args);
+	}
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio"); 
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
